47a4a64f256ddad3e9e0d51eede3edb2c6536718
jebw <jeb@jdwilkins.co.uk>
2009-01-10 18:04:48 +0100

    Renamed model template Get names of model to link to and expose to templates

--- generators/sage_download/sage_download_generator.rb
+++ generators/sage_download/sage_download_generator.rb
@@ -1,15 +1,27 @@
 class SageDownloadGenerator < Rails::Generator::Base
+  attr_accessor :customer_model, :order_model, :order_type
+
   def initialize(runtime_args, runtime_options = {})
     super
     
-    @file_name = "AddSageDownloadModel"
+    @order_type = (args.shift == 'sales_orders') ? 'sales_orders' : 'invoices'
+    
+    @customer_model = args.shift
+    @order_model = args.shift
   end
 
   def manifest
     record do |m|
       m.migration_template 'migration.rb', 'db/migrate', 
-                            :migration_file_name => "create_sage_downloads"
-      m.template 'model.rb', File.join('app', 'models', 'sage_download.rb')
+                           :migration_file_name => "create_sage_downloads"
+      m.template 'sage_download.rb', File.join('app', 'models', 'sage_download.rb')
     end
   end
+  
+  protected
+  
+    def banner
+      "Usage: #{$0} sage_download (invoices || sales_orders) CUSTOMERMODEL ORDERMODEL"
+    end
+    
 end
--- generators/sage_download/templates/sage_download.rb
+++ generators/sage_download/templates/sage_download.rb
@@ -1,7 +1,9 @@
 class SageDownload < ActiveRecord::Base
 
+  <%= order_type %> <%= order_model.underscore %> 
+
   def to_xml
     "<html><body><b>Hello</b> World</body></html>"
   end
-
+  
 end
