fbd7dccad78401b32f692554e2980d3f86ed82f4
jebw <jeb@jdwilkins.co.uk>
2009-01-12 19:50:30 +0100

    Added AttributeMapper module - needs a rethink since I'm having to hand it the correct map

--- lib/connect_to_sage.rb
+++ lib/connect_to_sage.rb
@@ -9,6 +9,9 @@ module ConnectToSage
   module ClassMethods
   
     def sage_invoice(fieldmap = {})
+      extend AttributeMapper
+      @@sage_invoice_map = fieldmap
+      
       has_and_belongs_to_many :sage_downloads
       belongs_to :sage_import
       
@@ -20,17 +23,34 @@ module ConnectToSage
     end
     
     def sage_sales_order(fieldmap = {})
+      extend AttributeMapper
+      @@sage_sales_order_map = fieldmap
+      
       has_and_belongs_to_many :sage_downloads
       belongs_to :sage_import
       
+      map_attribute(@@sage_sales_order_map, 'Id')
+      map_attribute(@@sage_sales_order_map, 'Forename') || raise(InvalidFormatError)
+      map_attribute(@@sage_sales_order_map, 'Surname')
+      map_attribute(@@sage_sales_order_map, 'RandomField') || raise(InvalidFormatError)
+      
       define_method "to_sales_order_xml" do |x|
         x.SalesOrder do
-          x.BarFoo
+          @@sage_sales_order_map.each do |k,v|
+            if v.is_a?(Symbol)
+              x.__send__ k, send(v)
+            else
+              x.__send__ k, v
+            end
+          end
         end
       end
     end
     
     def sage_customer(fieldmap = {})
+      extend AttributeMapper
+      @@sage_customer_map = fieldmap
+      
       has_and_belongs_to_many :sage_downloads
       belongs_to :sage_import
       
@@ -42,4 +62,26 @@ module ConnectToSage
     end
   
   end
+  
+  module AttributeMapper
+    def map_attribute(map, attribute, alternatives = [])
+      return map[attribute] if map.has_key?(attribute)
+      
+      needles = [ attribute, attribute.underscore, attribute.downcase ]
+      needles += alternatives
+      
+      needles.each do |needle|
+        if self.methods.include?(needle)
+          map[attribute] = needle.to_sym
+          return needle.to_sym
+        end
+      end
+      
+      nil
+    end
+  end
+  
+  class InvalidFormatError < RuntimeError
+  end
+  
 end
